7cb654a238e4d5c0404282d054f9bda2fe55443c
tschieber <tschieber@users.noreply.github.com>
2025-09-03 13:44:12 -0500

    Fix for environment variables.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    env: {
+        EMAIL_USER: process.env.EMAIL_USER,
+        EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+        EMAIL_RECIPIENT: process.env.EMAIL_RECIPIENT,
+    },
     images: {
         remotePatterns: [
             {
-                protocol: "https",
-                hostname: "picsum.photos",
-                pathname: "/**",
+                protocol: 'https',
+                hostname: 'picsum.photos',
+                pathname: '/**',
             },
         ],
     },
